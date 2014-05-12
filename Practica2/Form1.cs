45d10792b5aadae84f46b9021f84927d5b727e01
gchbascope <gchbascope1@yahoo.es>
2014-05-11 20:51:16 -0600

    Update Form1.cs

--- Practica2/Form1.cs
+++ Practica2/Form1.cs
@@ -19,10 +19,18 @@ namespace Practica2
         int addend1;
         int addend2;
 
-       // These ints will store the numbers
-	   // for the subtraction problem. 
-	   int minuend; 
-	   int subtrahend;
+        // These ints will store the numbers
+        // for the subtraction problem. 
+        int minuend; 
+	int subtrahend;
+	
+	// These ints will store the numbers for the multiplication problem.
+    	int multiplicand;
+   	int multiplier;
+
+    	// These ints will store the numbers for the division problem.
+    	int dividend;
+    	int divisor;
 	   
         // This int will keep track of the time left.
         int timeLeft;
@@ -36,12 +44,32 @@ namespace Practica2
         {
             addend1 = randomizer.Next(51);
             addend2 = randomizer.Next(51);
-
             plusLeftLabel.Text = addend1.ToString();
             plusRightLabel.Text = addend2.ToString();
-
             suma.Value = 0;
 
+	    // Fill in the subtraction problem.
+    	    minuend = randomizer.Next(1, 101);
+    	    subtrahend = randomizer.Next(1, minuend);
+   	    minusLeftLabel.Text = minuend.ToString();
+  	    minusRightLabel.Text = subtrahend.ToString();
+   	    diferencia.Value = 0;
+		 
+	    // Fill in the multiplication problem.
+   	    multiplicand = randomizer.Next(2, 11);
+   	    multiplier = randomizer.Next(2, 11);
+   	    timesLeftLabel.Text = multiplicand.ToString();
+   	    timesRightLabel.Text = multiplier.ToString();
+   	    producto.Value = 0;
+
+            // Fill in the division problem.
+   	    divisor = randomizer.Next(2, 11);
+   	    int temporaryQuotient = randomizer.Next(2, 11);
+   	    dividend = divisor * temporaryQuotient;
+   	    dividedLeftLabel.Text = dividend.ToString();
+   	    dividedRightLabel.Text = divisor.ToString();
+   	    cociente.Value = 0;
+   	    
             this.timeLeft = 30;
             this.timeLabel.Text = "30 seconds";
             timer1.Start();
@@ -77,12 +105,18 @@ namespace Practica2
                 timeLabel.Text = "Time's up!";
                 MessageBox.Show("You didn't finish in time.", "Sorry");
                 suma.Value = addend1 + addend2;
+                diferencia.Value = minuend - subtrahend;
+		producto.Value = multiplicand * multiplier;
+    		cociente.Value = dividend / divisor;
                 this.startButton.Enabled = true;
             }
         }
         private bool checkearRespuesta()
         {
-            if (addend1+addend2==suma.Value)
+            if ((addend1+addend2==suma.Value)
+            	 &&(minuend-subtrahend==diferencia.Value)
+            	 &&(multiplicand*multiplier==producto.Value)
+            	 &&(dividend/divisor==cociente.Value))
             {
                 return true;
             }
